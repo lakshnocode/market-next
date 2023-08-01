a018f1c7ae55c97eaed543eac38c3a434c8ab69c
akshat mishra <lakshya764@gmail.com>
2023-08-01 16:50:42 +0530

    first commit-d

--- next.config.cjs
+++ next.config.cjs
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.module.rules.push({
+          test: /\.(js|mjs|jsx|ts|tsx)$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['next/babel'],
+            },
+          },
+        });
+      }
+  
+      return config;
+    },
+  };
+  
