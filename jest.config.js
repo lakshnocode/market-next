3eeb63fae1726865dc9237341cb9f9b34b10f697
akshat mishra <lakshya764@gmail.com>
2023-08-01 17:01:22 +0530

    first commit-e

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,5 @@
-// module.exports = {
-//     transform: {
-//       '^.+\\.jsx?$': 'babel-jest',
-//     },
-//     moduleNameMapper: {
-//       '^.+\\.(css|less|scss)$': 'identity-obj-proxy',
-//     },
-//     transform: {
-//         '^.+\\.jsx?$': 'babel-jest'
-//       },
-//     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-//   };
-
-  export default {
-    testEnvironment: 'node',
-    transform: {},
-  };
-  
+module.exports = {
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+};
